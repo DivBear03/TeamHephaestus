12509eef1b249790f3335082c2fe1481cc759e36
DivBear03 <esben.n.nielsen.nj@gmail.com>
2022-12-24 13:07:10 -0500

    everything else

--- Project Files/Arduino/UNO/sketch_oct28b/sketch_oct28b.ino
+++ Project Files/Arduino/UNO/sketch_oct28b/sketch_oct28b.ino
@@ -35,7 +35,7 @@ Servo pusher;
 Servo rotator;
 //Stepper rotator(STEPS, 8, 9, 10, 11);
 
-double stepperPos[4] = {1000, 1150, 1300, 1450};
+double stepperPos[4] = {500, 1000, 1450, 2000};
 double stepperResult;
 String positions[8] = {"22.5", "67.5", "112.5", "157.5", "one", "two", "three", "four"};
 
@@ -46,9 +46,9 @@ signed int res;
 void setup()
 {
   pusher.attach(3);
-  pusher.writeMicroseconds(1200);
+  pusher.writeMicroseconds(1000);
   rotator.attach(5);
-  rotator.writeMicroseconds(1200);
+  rotator.writeMicroseconds(500);
   
   Serial.begin(9600);
   Serial.println("Program working");
@@ -58,30 +58,37 @@ void setup()
 
   recognizer.addSentence("Correct");
   recognizer.addSentence("Incorrect");
-//  recognizer.addSentence("Detonate");
-//  recognizer.addSentence("Go fuck yourself");
-//  recognizer.addSentence("Ready begin");
-//  recognizer.addSentence("hello");
-  recognizer.addSentence("em five bolt");
-  recognizer.addSentence("em six bolt");
-  recognizer.addSentence("em eight bolt");
-  recognizer.addSentence("em ten bolt");
+
+  recognizer.addSentence("servo savers");
+  recognizer.addSentence("screws");
+  recognizer.addSentence("em three nuts");
+  recognizer.addSentence("spacers");
+
+  recognizer.addSentence("Detonate");
+  recognizer.addSentence("Go fuck yourself");
+  recognizer.addSentence("Ready begin");
+  recognizer.addSentence("hello");
 
   recognizer.setThreshold(15);
 
   recognizer.train();                           // Train (may take 20seconds)
 }
 
+//void loop2() {
+//  while (!Serial.available()) {}
+//  String command = Serial.readStringUntil("\n");
+//  
+//  pusher.writeMicroseconds(command.toInt());
+//}
+
   void loop() {
     res = recognizer.poll();
     if (res != 0) {
-      Serial.println(res);
       if (res > 2 && res < 7) {
         // if is a recognized event
         stepperResult = stepperPos[res - 3];
-        Serial.print("Motor target: ");
-        Serial.println(stepperResult);
-        recognizer.say("Position " + positions[res+1] + " selected. Confirm selection please");
+        recognizer.say("Position three selected");
+        delay(2000);
         res = recognizer.poll();
         while (res != 1 && res != 2) {
           res = recognizer.poll();
@@ -90,19 +97,29 @@ void setup()
           recognizer.say("okay, selection confirmed");
           rotator.writeMicroseconds(stepperResult);
           delay(1000);
-          pusher.writeMicroseconds(1100); //  just above lowest position of ~900 or something
+          pusher.writeMicroseconds(1600); //  just above lowest position of ~900 or something
           delay(1000);
           pusher.writeMicroseconds(1000); // retract
         } else {
           recognizer.say("okay, canceling selection");
         }
+      } else if (res > 6 && res < 11) {
+        if (res == 3) {
+          recognizer.say("No. Detonate yourself. Actually, on second thought. huuuuuuh");
+        } else if (res == 4) {
+          recognizer.say("I refuse, you should go fuck yourself");
+        } else if (res == 5) {
+          recognizer.say("The Fitness Gram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues.");
+        } else if (res == 5) {
+          recognizer.say("Hello fellow mine craft villager. Huuuuuuuuh");
+        }
       }
     }
   }
-
-void resetServos() {
-
-  pusher.writeMicroseconds(0);
-  rotator.writeMicroseconds(0);
-  
-}
+//
+//void resetServos() {
+//
+//  pusher.writeMicroseconds(0);
+//  rotator.writeMicroseconds(0);
+//  
+//}
